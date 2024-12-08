1ac61283c2e89a002a9a127bd51b5d39c86d6c18
Allan Weyder <weyderallan03@gmail.com>
2024-12-07 22:31:47 -0300

    Adicionando os arquivos no dia 3

---
+++ Dia 3/index.js
@@ -0,0 +1,17 @@
+let nome = ""
+let idade = 0
+let altura = 0
+let peso = 0
+
+nome = prompt("Digite seu nome:")
+idade = prompt("Digite sua idade:")
+altura = prompt("Digite sua altura:")
+peso = prompt("Digite seu peso")
+
+let ano = 0
+ano = 2024 - idade
+
+let imc = 0
+imc = peso / altura * altura
+
+console.log("Olá " + nome + ", você tem " + idade + " anos, nasceu em " + ano + ", tem " + altura + " de altura e pesa " + peso + "kg. Seu IMC é " + imc + "Kg/m²")
