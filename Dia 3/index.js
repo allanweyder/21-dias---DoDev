1faf86ef8d391267b5e41e7c4ec50dd40fabe031
Allan Weyder <weyderallan03@gmail.com>
2024-12-09 20:06:09 -0300

    Adicionando arquivos dos dia 5

--- Dia 3/index.js
+++ Dia 3/index.js
@@ -14,5 +14,5 @@ ano = 2024 - idade
 let imc = 0
 imc = peso / altura * altura
 
-console.log("Olá " + nome + ", você tem " + idade + " anos, nasceu em " + ano + ", tem " + altura + " de altura e pesa " + peso + "kg. Seu IMC é " + imc + "Kg/m²")
+console.log("Olá " + nome + ", você tem " + idade + " anos, nasceu em " + ano + ", tem " + altura + " de altura e pesa " + peso + "kg. Seu IMC é " + imc + "Kg/m².S")
 
